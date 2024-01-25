c3a1a2aba3da2f93606f12c6fb417cea7cca68a7
irma-suppes <irma.suppes.a@gmail.com>
2024-01-25 17:30:46 +0300

    Refactor token blocking

--- blocking.py
+++ blocking.py
@@ -1,7 +1,5 @@
-from utils import get_candidate_pairs_between_blocks, get_words_ngrams
+from utils import get_candidate_pairs_between_blocks, get_words_ngrams, get_token_blocks
 from collections import defaultdict
-from nltk.tokenize import word_tokenize
-import pandas as pd
 import itertools
 import numpy as np
 from nltk.corpus import stopwords
@@ -19,10 +17,10 @@ def blocking(df1, df2, blocking_scheme, params):
         blocks = token_blocking(df1[['title_acm', 'authors_acm']], df2[['title_dblp', 'authors_dblp']], stop_words)
         blocks = np.unique(blocks, axis=0)
         return blocks
-    elif blocking_scheme == 'st':
+    elif blocking_scheme == 'by_year':
         blocks = blocking_by_year(df1, df2)
         return blocks
-        
+
 
 def create_ngram_word_blocks(df, column, n):
     blocks = {}
@@ -31,58 +29,29 @@ def create_ngram_word_blocks(df, column, n):
         for ngram in ngrams_list:
             if ngram not in blocks:
                 blocks[ngram] = []
-            blocks[ngram].append(idx) # only append idx of row in dataframe to save more space
+            blocks[ngram].append(idx)  # only append idx of row in dataframe to save more space
     return blocks
 
+
 def token_blocking(df1, df2, stop_words: set):
-    blocks1 = defaultdict(list)
-    blocks2 = defaultdict(list)
-
-    for idx, row in enumerate(df1.itertuples()):
-
-        string = " ".join([str(value) for value in row if not pd.isna(value)])
-        tokens = set(
-            [word for word in word_tokenize(string) if word not in stop_words]
-        )
-
-        for token in tokens:
-            blocks1[token].append(idx)
-
-    for idx, row in enumerate(df2.itertuples()):
-
-        string = " ".join([str(value) for value in row if not pd.isna(value)])
-        tokens = set(
-            [word for word in word_tokenize(string) if word not in stop_words]
-        )
-
-        for token in tokens:
-            blocks2[token].append(idx)
-
-    blocks1 = {
-        key: indices
-        for key, indices in blocks1.items()
-        if len(indices) < 1000 and len(indices) > 1
-    }
-    blocks2 = {
-        key: indices
-        for key, indices in blocks2.items()
-        if len(indices) < 1000 and len(indices) > 1
-    }
+    blocks1 = get_token_blocks(df1, stop_words)
+    blocks2 = get_token_blocks(df2, stop_words)
 
     pairs = [list(pair) for key in (blocks1.keys() & blocks2.keys()) for pair in
              list(itertools.product(blocks1[key], blocks2[key]))]
 
     return np.array(pairs)
 
-def blocking_by_year(df1, df2, cols=['year_acm', 'year_dblp']):
+
+def blocking_by_year(df1, df2):
     b1 = defaultdict(list)
     b2 = defaultdict(list)
 
-    for idx, key in df1[cols[0]].items():
+    for idx, key in df1['year_acm'].items():
         if key:
             b1[key].append(idx)
 
-    for idx, key in df2[cols[1]].items():
+    for idx, key in df2['year_dblp'].items():
         if key:
             b2[key].append(idx)
 
--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
 import csv
+from collections import defaultdict
+
+import pandas as pd
+from nltk import word_tokenize
+
 from data_loading import load_two_publication_sets
+
 
 def get_symbol_ngrams(text, number=3):
     if not text:
@@ -10,6 +16,7 @@ def get_symbol_ngrams(text, number=3):
         ngrams.add(text[x:x + number])
     return ngrams
 
+
 # Function to create n-gram blocks for a dataframe
 def get_words_ngrams(text, n):
     words = text.split()
@@ -18,7 +25,8 @@ def get_words_ngrams(text, n):
     ngrams_list = [tuple(words[i:i + n]) for i in range(len(words) - n + 1)]
 
     # list containing strings (which are basically n subsequent words)
-    return [' '.join(gram) for gram in ngrams_list]  
+    return [' '.join(gram) for gram in ngrams_list]
+
 
 # HELPER TO SEE WHETHER MATCHED INDICES ARE CORRECT
 def show_tuples_behind_indices_pair(filename, newfilename):
@@ -35,10 +43,11 @@ def show_tuples_behind_indices_pair(filename, newfilename):
             idx1 = int(row[0])
             idx2 = int(row[1])
 
-            real_pairs.append((df1.iloc[idx1]["Combined"],df2.iloc[idx2]["Combined"]))
+            real_pairs.append((df1.iloc[idx1]["Combined"], df2.iloc[idx2]["Combined"]))
         columns = ["Combined_1", "Combined_2"]
         df_matches = pd.DataFrame(real_pairs, columns=columns)
         df_matches.to_csv(newfilename, encoding='utf-8-sig', index=False)
+
 
 def get_candidate_pairs_between_blocks(blocks1, blocks2):
     """
@@ -48,5 +57,25 @@ def get_candidate_pairs_between_blocks(blocks1, blocks2):
     for ngram in blocks1:
         if ngram in blocks2:
             pairs = [(id1, id2) for id1 in blocks1[ngram] for id2 in blocks2[ngram]]
-            candidate_pairs.update(pairs) # add all candidate pairs to set
+            candidate_pairs.update(pairs)  # add all candidate pairs to set
     return candidate_pairs
+
+
+def get_token_blocks(df, stop_words):
+    blocks = defaultdict(list)
+
+    for idx, row in enumerate(df.itertuples()):
+        string = " ".join([str(value) for value in row if not pd.isna(value)])
+        tokens = set(
+            [word for word in word_tokenize(string) if word not in stop_words]
+        )
+        for token in tokens:
+            blocks[token].append(idx)
+
+    blocks = {
+        key: indices
+        for key, indices in blocks.items()
+        if 1000 > len(indices) > 1
+    }
+
+    return blocks
