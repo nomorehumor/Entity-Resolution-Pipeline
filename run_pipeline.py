62e00011ced3195fab19ac4f87a2e054fa117502
irma-suppes <irma.suppes.a@gmail.com>
2024-02-02 21:37:51 +0100

    Small fixes

--- run_pipeline.py
+++ run_pipeline.py
@@ -18,15 +18,16 @@ def write_dataset(df, path, columns_suffix):
             f'Combined_{columns_suffix}': 'Combined'
             }).to_csv(path, index=False)
 
+
 def run_entity_resolution(df_acm, df_dblp, blocking_function, matching_function, sim_threshold, blocking_params=None, matching_params={}):
     print(f'Running entity resolution with blocking function {blocking_function} and matching function {matching_function}')
-    pipeline_start= time.time()
+    pipeline_start = time.time()
     # Blocking
     pairs = blocking(df_acm, df_dblp, blocking_scheme=blocking_function, params=blocking_params)
     
     # Matching
     df_pairs = matching(df_acm, df_dblp, pairs, matching_function, weights=matching_params.get('matching_weights'))
-    df_pairs[["index_acm", "paperId_acm", "index_dblp", "paperId_dblp"]].to_csv(f'{OUTPUT_DIR}/Matched_Entities.csv', index=False)
+    df_pairs[df_pairs.similarity > sim_threshold][["index_acm", "paperId_acm", "index_dblp", "paperId_dblp"]].to_csv(f'{OUTPUT_DIR}/Matched_Entities.csv', index=False)
     pipeline_end = time.time()
     print(f'Time needed for blocking and matching: {pipeline_end-pipeline_start}')
 
@@ -38,7 +39,8 @@ def run_entity_resolution(df_acm, df_dblp, blocking_function, matching_function,
     write_dataset(df_acm_dedup, f'{OUTPUT_DIR}/ACM_deduplicated.csv', "acm")
     write_dataset(df_dblp_dedup, f'{OUTPUT_DIR}/DBLP_deduplicated.csv', "dblp")    
 
+
 if __name__ == "__main__":
     df_acm, df_dblp = load_two_publication_sets()
 
-    run_entity_resolution(df_acm, df_dblp, "ngram_word_blocks", "levenshtein", 0.8, {"n":4}, {"matching_weights": [0.3, 0.3, 0.3]})
+    run_entity_resolution(df_acm, df_dblp, "ngram_word_blocks", "levenshtein", 0.8, {"n":3}, {"matching_weights": [0.33, 0.33, 0.33]})
